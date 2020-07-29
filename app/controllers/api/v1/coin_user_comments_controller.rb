1822849b79e9b90f58b86538164364d7168a837e
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-07-29 15:57:10 -0400

    working on comments

--- app/controllers/api/v1/coin_user_comments_controller.rb
+++ app/controllers/api/v1/coin_user_comments_controller.rb
@@ -2,22 +2,34 @@ class Api::V1::CommentsController < ApplicationController
   class Api::V1::CoinUserCommentsController < ApplicationController
 
     def index
+        puts params
         comments = CoinUserComment.all
         render json:comments
     end
 
     def create
-      @current_user = User.find_by(user_name: params[:coinObj][:"\"user_name\""])
+
+      @current_user = User.find_by(user_name: params[:username])
+        symbols = Coin.all.map { |e| e.symbol }
+     puts symbols
+     
+         if symbols.include?(params[:symbol])
+        @coin = Coin.find_by(symbol: params[:symbol])
+         else
+        @coin = Coin.create(symbol: params[:symbol])
+         end
 
         @comment = CoinUserComment.create(
-          coin_id: params[:coinObj][:"\"id\""],
-          user_id: @current_user.id
+          coin_id: @coin.id,
+          user_id: @current_user.id,
+          comment: params[:comment]
         )
-        @current_user.coin_user_comments.push(@comment)
-        render json: @current_user.coin_user_comments
-      end
 
+        @current_user.coin_user_comments.push(@comment)
+        @coin.coin_user_comments.push(@comment)
+        render json: @coin.coin_user_comments
     end
+
 
     def show
 
--- db/migrate/20200724151822_create_coin_user_comments.rb
+++ db/migrate/20200724151822_create_coin_user_comments.rb
@@ -1,8 +1,8 @@
 class CreateCoinUserComments < ActiveRecord::Migration[6.0]
   def change
     create_table :coin_user_comments do |t|
+      t.integer :coin_id
       t.integer :user_id
-      t.text :coin_id
       t.text :comment
       t.timestamps
     end
