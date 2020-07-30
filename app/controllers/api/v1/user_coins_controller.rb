76d8f6308d85b34c23b3e8dd7837fb19932c3ee4
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-07-30 17:19:59 -0400

    auth bug fixes

--- app/controllers/api/v1/user_coins_controller.rb
+++ app/controllers/api/v1/user_coins_controller.rb
@@ -5,14 +5,28 @@ class Api::V1::UserCoinsController < ApplicationController
     @current_user = User.find_by(user_name: params['user_name'])
 
     if @current_user.coins.map{|s| s.symbol}.include?(params['symbol'])
-    render json: {"message": "Already tracking!"}
+      response = []
+
+      response.push(@current_user)
+      response.push(@current_user.coins)
+
+      token = encode_token(user_id: @current_user.id)
+      response.push(token)
+
+      error = "Already tracking"
+      response.push(error)
+    render json: response
     else
       Coin.find_by(symbol: params['symbol']) ? @coin = Coin.find_by(symbol: params['symbol']) : @coin = Coin.create(symbol: params['symbol'])
 
       @current_user.coins.push(@coin)
       response = []
+
       response.push(@current_user)
       response.push(@current_user.coins)
+
+      token = encode_token(user_id: @current_user.id)
+      response.push(token)
     render json: response
     end
   end
@@ -39,9 +53,18 @@ class Api::V1::UserCoinsController < ApplicationController
         response.push(@current_user)
         response.push(@current_user.coins)
 
+        token = encode_token(user_id: @current_user.id)
+        response.push(token)
+
         render json: response
       else
-      response = ["not currently tracking!"]
+        response = []
+        response.push(@current_user)
+        response.push(@current_user.coins)
+
+        token = encode_token(user_id: @current_user.id)
+        response.push(token)
+
       render json: response
       end
 
@@ -50,7 +73,7 @@ class Api::V1::UserCoinsController < ApplicationController
   private
 
   def user_coins_params
-   params.require(:coinObj).permit!
+   params.require(:coin).permit!
   end
 
 
